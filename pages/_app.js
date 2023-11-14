48c2c0e169e1883874da9069ab6d146b6ab2969e
Alexander <stwork87@gmail.com>
2023-11-14 15:52:53 +0300

    added GA4

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,30 @@
+import Script from 'next/script';
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+      <>
+        <Script
+          strategy="afterInteractive"
+          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}`}
+        />
+        <Script
+          id="gtag-init"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', '${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}', {
+                page_path: window.location.pathname,
+              });
+            `,
+          }}
+        />
+        <Component {...pageProps} />
+      </>
+    );
 }
 
 export default MyApp
